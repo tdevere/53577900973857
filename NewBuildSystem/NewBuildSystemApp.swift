534b43717a46854b1b8f879669c9346d3ed3c773
tdevere <tdevere@microsoft.com>
2022-02-25 16:45:15 -0600

    added code

---
+++ NewBuildSystem/NewBuildSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewBuildSystemApp.swift
+//  NewBuildSystem
+//
+//  Created by Tony DeVere on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct NewBuildSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
